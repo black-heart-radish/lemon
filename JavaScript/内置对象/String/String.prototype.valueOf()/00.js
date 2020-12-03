a61db6b6f55589686d9652f5e6cb1f83f7fde174
TortoiseGit <2634893889@qq.com>
2020-12-03 13:02:17 +0800

    String.prototype.valueOf()

---
+++ JavaScript/内置对象/String/String.prototype.valueOf()/00.js
@@ -0,0 +1,6 @@
+// String.prototype.valueOf() 返回 String 对象的原始值
+//   此方法通常由JavaScript在内部调用，而不是在代码中显式调用。
+//     语法 stringObject.valueOf()
+
+let x = new String('Hello world');
+console.log(x.valueOf()); // 'Hello world'
