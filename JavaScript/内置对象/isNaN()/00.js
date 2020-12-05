3d30d1dbd9a34525c684275f6b8e3a0e455257c1
TortoiseGit <2634893889@qq.com>
2020-12-05 20:12:34 +0800

    isNaN()

---
+++ JavaScript/内置对象/isNaN()/00.js
@@ -0,0 +1,9 @@
+// isNaN() 判断一个值是否为 NaN
+//     语法： isNaN(value)
+//     为 NaN , 返回 true
+//     不为， 返回 false
+
+let NaN1 = NaN
+let num1 = 123
+console.log(isNaN(NaN1)); // true 
+console.log(isNaN(num1)); // false
---
+++ JavaScript/内置对象/isNaN()/readme.md
@@ -0,0 +1,3 @@
+# isNaN() 判断一个值是否为NaN
+
+## 这玩意儿与 Number.isNaN() 一毛一样
