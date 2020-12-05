847696a6fba883ebc8f61c802f97dc0d6179c066
TortoiseGit <2634893889@qq.com>
2020-12-05 22:09:08 +0800

    Boolean.prototype.toString()

---
+++ JavaScript/内置对象/Boolean/Boolean.prototype.toString()/00.js
@@ -0,0 +1,10 @@
+// Boolean.prototype.toString() 返回字符串形式
+//     语法： booleanObject.toString()
+
+let boolean1 = true
+let boolean2 = false
+let str = boolean1.toString()
+console.log(boolean1.toString()); // true
+console.log(boolean2.toString()); // false
+console.log(typeof(boolean1)); // boolean
+console.log(typeof(str)); // string
---
+++ JavaScript/内置对象/Boolean/Boolean.prototype.toString()/readme.md
@@ -0,0 +1,7 @@
+# Boolean.prototype.toString()
+
+## 值0，-0，null，false，NaN，undefined，或空字符串（""）的对象 转化为 Boolean 为 false
+
+## 所有其他值，包括任何对象，空数组（[]）或字符串"false" 的对象 转化为 Boolean 为 true
+
+## Boolean.prototype.toString() 将布尔值转化为 string , true or false
