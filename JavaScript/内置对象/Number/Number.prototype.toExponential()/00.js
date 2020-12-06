ecc486fa0d9612efd989b4a13fc01b4e2b12263a
TortoiseGit <2634893889@qq.com>
2020-12-06 15:10:08 +0800

    Number.prototype.toExponential()

---
+++ JavaScript/内置对象/Number/Number.prototype.toExponential()/00.js
@@ -0,0 +1,9 @@
+// Number.prototype.toExponential() 返回指定 小数位数 的 科学记数 的 字符串
+//     语法： numObject.toExponential(fractionDigits)
+//       fractionDigits 小数的位数 0 ~ 20
+//     也会四舍五入
+
+let num1 = 121212121212;
+let num2 = 0.121212121212;
+console.log(num1.toExponential(5)); // 1.21212e+11
+console.log(num2.toExponential(5)); // 1.21212e-1
---
+++ JavaScript/内置对象/Number/Number.prototype.toExponential()/readme.md
@@ -0,0 +1,5 @@
+# Number.prototype.toExponential()
+
+## 返回指定小数位( 0 ~ 20)的 科学记数法的 字符串
+
+## 也会四舍五入
